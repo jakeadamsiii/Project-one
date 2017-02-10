ee0e4e9d84950cd7cd54207d315cf970ef2b8ab3
Jake Adams <jadams.html@gmail.com>
2017-02-10 14:26:09 +0000

    battle screen and fight button

--- js/app.js
+++ js/app.js
@@ -2,10 +2,22 @@ $(()=> {
   console.log('hello');
   const $pressStart = $('#start');
   const $battleScreen = $('#battle');
+  const $fight = $('#fight');
+  let yourHP =50;
+  let opponentsHP =50;
 
+  function fight(){
+    if (yourHP > 0 && opponentsHP>0){
+
+    }
+  }
+
+  //scroll from landing page to battle page
   function scroll(){
     $('html,body').animate({scrollTop: $battleScreen.offset().top},'slow');
   }
+
+
 
   $pressStart.on('click', scroll);
 
