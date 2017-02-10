2fcda3976fb2359b615ac188e4e1831cdad6a107
Jake Adams <jadams.html@gmail.com>
2017-02-10 14:58:53 +0000

    Added audio

--- js/app.js
+++ js/app.js
@@ -3,8 +3,14 @@ $(()=> {
   const $pressStart = $('#start');
   const $battleScreen = $('#battle');
   const $fight = $('#fight');
+  const $startTheme= $('#startTheme');
+  const $battleTheme= $('#battleTheme');
+
   let yourHP =50;
   let opponentsHP =50;
+
+  console.log($startTheme);
+  console.log($battleTheme);
 
   function fight(){
     if (yourHP > 0 && opponentsHP>0){
@@ -15,10 +21,20 @@ $(()=> {
   //scroll from landing page to battle page
   function scroll(){
     $('html,body').animate({scrollTop: $battleScreen.offset().top},'slow');
+    battleMusic();
+  }
+
+  function battleMusic(){
+    $startTheme[0].pause();
+    $battleTheme[0].play();
+  }
+
+  function attacks(){
+
   }
 
 
-
   $pressStart.on('click', scroll);
+  $fight.on('click', attacks);
 
 });
