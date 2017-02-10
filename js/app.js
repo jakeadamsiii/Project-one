fd95c2a9a87b31f6cc4e4a948e948771f5edae70
Jake Adams <jadams.html@gmail.com>
2017-02-10 12:59:18 +0000

    title screen css

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log('JS loaded');
+$(()=> {
+  console.log('hello');
+  const $pressStart = $('#start');
+  const $battleScreen = $('#battle');
+
+  function scroll(){
+    $('html,body').animate({scrollTop: $battleScreen.offset().top},'slow');
+  }
+
+  $pressStart.on('click', scroll);
+
+});
